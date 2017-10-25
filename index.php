85a69aac9d4446bdc0a72fcaa7c2282efafb7a9c
pancho <francisco.barriosr@gmail.com>
2017-10-25 15:45:16 -0300

    compatibilidad wordpress

--- index.php
+++ index.php
@@ -11,31 +11,31 @@
       gtag('config', 'UA-107004784-1');
     </script>
  -->    
-    <link rel="apple-touch-icon" sizes="57x57" href="icons/icon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="icons/icon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="icons/icon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="icons/icon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="icons/icon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="icons/icon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="icons/icon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="icons/icon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="icons/icon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="icons/icon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="icons/icon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="icons/icon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="icons/icon/favicon-16x16.png">
-    <link rel="manifest" href="icons/icon/manifest.json">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo('stylesheet_directory')?>/icons/icon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/favicon-16x16.png">
+    <link rel="manifest" href="<?php bloginfo('stylesheet_directory')?>/icons/icon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="icons/icon/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="<?php bloginfo('stylesheet_directory')?>/icons/icon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
     <title>Procelar</title>
-    <link rel="stylesheet" href="css/foundation-flex.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="icons/foundation-icons.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory')?>/css/foundation-flex.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory')?>/style.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory')?>/icons/foundation-icons.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 </head>
 <body>
@@ -46,7 +46,7 @@
           <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium" style="background:transparent;">
             <div class="columns small-8 large-3 ">
                 <ul class="menu">
-                   <img src="assets/procelarLogo.svg">
+                   <img src="<?php bloginfo('stylesheet_directory')?>/assets/procelarLogo.svg">
                 </ul>
             </div>
             <div>
@@ -81,9 +81,9 @@
      <div data-sticky-container class="show-for-medium" style="min-height:  120px;">
       <div class="top-bar headermenu" data-sticky data-options="marginTop: 0" id="example-menu">
         <div class="row">
-          <div class="columns small-5  large-3 show-for-medium"" style="width: 250px;">
+          <div class="columns small-5  large-3 show-for-medium" style="width: 250px;">
             <ul class="menu">
-               <img src="assets/procelarLogo.svg" style="max-width: 200px">
+               <img src="<?php bloginfo('stylesheet_directory')?>/assets/procelarLogo.svg" style="max-width: 200px">
             </ul>
           </div>
           <div class="columns small-12 medium-9 show-for-medium" >
@@ -102,7 +102,7 @@
 
     
     <div>
-       <img class="imgOrbit" style="width: 100%; height:auto;" data-interchange="[assets/Mobile@2x.png, small], [assets/Tablet.png, medium], [assets/Desktop.png, large]]">  
+       <img class="imgOrbit" style="width: 100%; height:auto;" data-interchange="[<?php bloginfo('stylesheet_directory')?>/assets/Mobile@2x.png, small], [<?php bloginfo('stylesheet_directory')?>/assets/Tablet.png, medium], [<?php bloginfo('stylesheet_directory')?>/assets/Desktop.png, large]]">  
     </div> 
 
     <div class="sections">
@@ -115,7 +115,7 @@
                 </div>
                  <div class="small-12 medium-6 columns card2 align-self-bottom" style="padding-bottom: 40px;">
 
-                        <img  height="681px" width="681px" src="assets/producto.png"  alt=""  />    
+                        <img  height="681px" width="681px" src="<?php bloginfo('stylesheet_directory')?>/assets/producto.png"  alt=""  />    
                  
                 </div>
           </div>
@@ -128,7 +128,7 @@
             <div class="card columns">
               <article>  
                 <picture>
-                  <img src="assets/btnOk.svg" id="imageCard"/>  
+                  <img src="<?php bloginfo('stylesheet_directory')?>/assets/btnOk.svg" id="imageCard"/>  
                 </picture>
                   
                 <div class="card-section">
@@ -144,7 +144,7 @@
             <div class="card columns">
               <article>  
                 <picture>
-                    <img src="assets/btnTime.svg"  id="imageCard"/>  
+                    <img src="<?php bloginfo('stylesheet_directory')?>/assets/btnTime.svg"  id="imageCard"/>  
                  </picture>
                   
                 <div class="card-section">
@@ -159,7 +159,7 @@
             <div class="card columns">
               <article>  
                 <picture>
-                    <img src="assets/btnTime2.svg"  id="imageCard"/>  
+                    <img src="<?php bloginfo('stylesheet_directory')?>/assets/btnTime2.svg"  id="imageCard"/>  
                 </picture>
                   
                 <div class="card-section">
@@ -174,7 +174,7 @@
       
       <section">
         <div>
-          <img class="imgOrbit" style="width: 100%; height:auto;" data-interchange="[assets/puntosVentasMobile@2x.jpg, small], [assets/puntosVentasTablet.jpg, medium], [assets/puntosVentas.jpg, large]], [assets/puntosVentasHD.jpg, retina]]">  
+          <img class="imgOrbit" style="width: 100%; height:auto;" data-interchange="[<?php bloginfo('stylesheet_directory')?>/assets/puntosVentasMobile@2x.jpg, small], [<?php bloginfo('stylesheet_directory')?>/assets/puntosVentasTablet.jpg, medium], [<?php bloginfo('stylesheet_directory')?>/assets/puntosVentas.jpg, large]], [<?php bloginfo('stylesheet_directory')?>/assets/puntosVentasHD.jpg, retina]]">  
        </div> 
         
       </section>
@@ -189,7 +189,7 @@
             <div class="columns small-12">
               <div class="responsive-embed">
                    <div class="columns small-12 text-center" >
-                      <iframe id ="videoFrame" width="960" height="540" src="assets/video.mp4" frameborder="0" allowfullscreen></iframe>
+                      <iframe id ="videoFrame" width="960" height="540" src="<?php bloginfo('stylesheet_directory')?>/assets/video.mp4" frameborder="0" allowfullscreen></iframe>
                   </div> 
               </div> 
             </div>
@@ -204,13 +204,13 @@
           <h5 class="textSpecial" style=" line-height: 1.2; ">Encuéntranos <br> en las principales farmacias del país</h5>
             <div class="row medium-up-3 " >
               <div class="small-12 medium-4 columns align-self-middle" id="imagesBrand" style="padding-bottom: 20px;">
-                 <img  width="auto" height="auto" src="assets/cruzVerde.jpg"  />  
+                 <img  width="auto" height="auto" src="<?php bloginfo('stylesheet_directory')?>/assets/cruzVerde.jpg"  />  
               </div>
               <div class="small-12 medium-4 columns align-self-middle" id="imagesBrand" style="padding-bottom: 20px;" >
-                 <img  width="auto" height="auto" src="assets/ahumada.jpg"  />  
+                 <img  width="auto" height="auto" src="<?php bloginfo('stylesheet_directory')?>/assets/ahumada.jpg"  />  
               </div>
               <div class="small-12 medium-4 columns align-self-middle" id="imagesBrand" style="padding-bottom: 20px;">
-                 <img  width="auto" height="auto" src="assets/salco.png"  />  
+                 <img  width="auto" height="auto" src="<?php bloginfo('stylesheet_directory')?>/assets/salco.png"  />  
               </div>
             </div> 
           </div>   
@@ -223,16 +223,16 @@
         <footer class="footer row align-justify">
             <div class="small-3 column hide-for-small-only">
                 <div class="row">
-                    <img src="assets/procelarLogo.svg" class="attachment-full size-full wp-post-image" alt="">          
+                    <img src="<?php bloginfo('stylesheet_directory')?>/assets/procelarLogo.svg" class="attachment-full size-full wp-post-image" alt="">          
                 </div>
                <div class="row footerSocial">
                     <div class="small-12 medium-4 columns align-self-middle"">
                         <a href="https://www.facebook.com/ProcelarCL/"> 
-                            <img src="assets/facebookLogo.svg" class="attachment-full size-full wp-post-image" alt="www.facebook.com" style="padding-bottom: 20px; height: 60px; width: 60px;"></a>
+                            <img src="<?php bloginfo('stylesheet_directory')?>/assets/facebookLogo.svg" class="attachment-full size-full wp-post-image" alt="www.facebook.com" style="padding-bottom: 20px; height: 60px; width: 60px;"></a>
                     </div>
                     <div class="small-12 medium-8 columns align-self-middle text-align-center">
                          <a href="https://www.instagram.com/procelarcl"> 
-                            <img src="assets/instagramLogo.svg" class="attachment-full size-full wp-post-image" alt="www.instagram.com" style="padding-bottom: 20px; height: 60px; width: 60px;"></a>
+                            <img src="<?php bloginfo('stylesheet_directory')?>/assets/instagramLogo.svg" class="attachment-full size-full wp-post-image" alt="www.instagram.com" style="padding-bottom: 20px; height: 60px; width: 60px;"></a>
                     </div>
                 </div>
             </div>
@@ -266,7 +266,7 @@
                         <p> Teléfono:(2) 2340 5800 </p>
                     </div>
                     <div class="columns small-3">
-                      <img src="assets/pharmaLogo.svg" class="attachment-full size-full wp-post-image" alt="">
+                      <img src="<?php bloginfo('stylesheet_directory')?>/assets/pharmaLogo.svg" class="attachment-full size-full wp-post-image" alt="">
                       </div>
                   </div>
                 </div>
@@ -276,7 +276,7 @@
     <div class="footer-container show-for-small-only">
         <footer class="footer row align-justify">
             <div class="columns small-12 show-for-small-only">
-                <img src="assets/procelarLogo.svg" class="attachment-full size-full wp-post-image" alt="">
+                <img src="<?php bloginfo('stylesheet_directory')?>/assets/procelarLogo.svg" class="attachment-full size-full wp-post-image" alt="">
             </div>
             <div class="columns small-12 show-for-small-only" id="menuFooterMobile">
                 <ul id="menu-footer-1" class="menu footermenu vertical menu">
@@ -310,29 +310,29 @@
                         <div class="row">
                             <div class="small-6 columns text-center" id="imgFacebookMobile">
                             <a href="https://www.facebook.com/ProcelarCL/"> 
-                                <img src="assets/facebookLogo.svg" class="attachment-full size-full wp-post-image" alt="www.facebook.com" style="height: 60px; width: 60px;">   
+                                <img src="<?php bloginfo('stylesheet_directory')?>/assets/facebookLogo.svg" class="attachment-full size-full wp-post-image" alt="www.facebook.com" style="height: 60px; width: 60px;">   
                              </a>
                             </div>
                              <div class="small-6 columns text-center">
                                  <a href="https://www.instagram.com/procelarcl"> 
-                                    <img src="assets/instagramLogo.svg" class="attachment-full size-full wp-post-image" alt="www.instagram.com" style="height: 60px; width: 60px;">
+                                    <img src="<?php bloginfo('stylesheet_directory')?>/assets/instagramLogo.svg" class="attachment-full size-full wp-post-image" alt="www.instagram.com" style="height: 60px; width: 60px;">
                                   </a>
                              </div>
                         </div>
                     </div>
                 </div>
                 <div class="columns small-12 footermenu text-center" id="imgLogoFooter2Mobile">
-                      <img src="assets/pharmaLogo.svg" class="attachment-full size-full wp-post-image" alt="" style="margin-top: 20px;"> 
+                      <img src="<?php bloginfo('stylesheet_directory')?>/assets/pharmaLogo.svg" class="attachment-full size-full wp-post-image" alt="" style="margin-top: 20px;"> 
                 </div>     
             </div>
         </footer>
     </div>
  
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.js"></script>
-    <script src="js/vendor/foundation.interchange.js"></script>
-    <script src="js/app.js"></script>
+    <script src="<?php bloginfo('stylesheet_directory')?>/js/vendor/jquery.js"></script>
+    <script src="<?php bloginfo('stylesheet_directory')?>/js/vendor/what-input.js"></script>
+    <script src="<?php bloginfo('stylesheet_directory')?>/js/vendor/foundation.js"></script>
+    <script src="<?php bloginfo('stylesheet_directory')?>/js/vendor/foundation.interchange.js"></script>
+    <script src="<?php bloginfo('stylesheet_directory')?>/js/app.js"></script>
   
 
 </body>
